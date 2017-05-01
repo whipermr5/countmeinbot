e4e4dd032938d152417afc33aaa23798f252bfbb
John Yong <whipermr5@gmail.com>
2017-05-01 23:51:16 +0800

    Implement poll author details in polls page; add separator after each poll

--- main.py
+++ main.py
@@ -418,13 +418,24 @@ class MigratePage(webapp2.RequestHandler):
 
 class PollsPage(webapp2.RequestHandler):
     def get(self):
+        from datetime import timedelta
         cursor = self.request.get('cursor', None)
         if cursor:
             cursor = ndb.query.Cursor(urlsafe=cursor)
         query = Poll.query().order(-Poll.created)
         polls, next_cursor, has_more = query.fetch_page(1000, start_cursor=cursor)
         for poll in polls:
-            self.response.write('<p>' + poll.render_text().replace('\n', '<br>\n') + '</p>\n\n')
+            poll_text = poll.render_text()
+            idx = poll_text.find('\n')
+            user = get_user(int(poll.admin_uid))
+            if user:
+                user_description = user.get_description()
+            else:
+                user_description = u'unknown ({})'.format(poll.admin_uid)
+            timestamp = (poll.created + timedelta(hours=8)).strftime('%a, %d %b \'%y, %H:%M:%S')
+            poll_details = u' <small>by {} on {}</small>'.format(user_description, timestamp)
+            poll_text = (poll_text[:idx] + poll_details + poll_text[idx:])
+            self.response.write('<p>' + poll_text.replace('\n', '<br>\n') + '</p>\n\n<hr>\n\n')
         if not has_more:
             return
         self.response.write('<p><a href="?cursor={}">More</a></p>'.format(next_cursor.urlsafe()))
