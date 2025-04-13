e2d29ff85339ad765f0ca7d61d2168516f52d2f1
John Yong <whipermr5@gmail.com>
2025-04-13 14:53:17 +0800

    Revert "Migrate polls in batches via admin polls page"
    This reverts commit df79b5fd97e0578960e10a87ba751f4f67e07bc4.
    
    The data migration from pickle to json is now complete :)

--- admin.py
+++ admin.py
@@ -5,14 +5,9 @@ from model import Poll
 import webapp2
 from google.appengine.ext.ndb.query import Cursor
 from google.appengine.api.datastore_errors import BadValueError
-from google.appengine.api import taskqueue
 
 class MigratePage(webapp2.RequestHandler):
-    def post(self, pid):
-        Poll.migrate(int(pid))
-
-    def get(self, pid):
-        taskqueue.add(queue_name='outbox', url='/migrate/{}'.format(pid))
+    def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Migrate page\n')
 
@@ -31,12 +26,6 @@ class PollPage(webapp2.RequestHandler):
 
 class PollsPage(webapp2.RequestHandler):
     def get(self):
-        self.process_polls()
-
-    def post(self):
-        self.process_polls()
-
-    def process_polls(self):
         try:
             cursor = Cursor.from_websafe_string(self.request.get('cursor'))
         except BadValueError:
@@ -47,17 +36,16 @@ class PollsPage(webapp2.RequestHandler):
             if limit <= 0:
                 raise ValueError
         except (TypeError, ValueError):
-            limit = 1000
+            limit = 100
 
-        query = Poll.query().order(Poll.created)
-        polls, next_cursor, has_more = query.fetch_page(limit, start_cursor=cursor, keys_only=True)
+        query = Poll.query().order(-Poll.created)
+        polls, next_cursor, has_more = query.fetch_page(limit, start_cursor=cursor)
 
         for poll in polls:
-            taskqueue.add(queue_name='outbox', url='/migrate/{}'.format(poll.id()))
+            self.response.write(poll.render_html() + '\n\n<hr>\n\n')
 
         if not has_more:
             return
 
         more_url = '?cursor={}&limit={}'.format(next_cursor.to_websafe_string(), limit)
         self.response.write('<p><a href="{}">More</a></p>'.format(more_url))
-        taskqueue.add(queue_name='outbox', url='/polls', params={'cursor': next_cursor.to_websafe_string(), 'limit': limit})
